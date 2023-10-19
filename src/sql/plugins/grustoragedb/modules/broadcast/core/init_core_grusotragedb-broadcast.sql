bc17f7b1611a24e7742b63faf17a03675c3b6a51
seboo <25958061+seboo@users.noreply.github.com>
2023-10-19 10:42:46 +0200

    GRUSTDBBRD-01 : update swagger and sql init

--- src/sql/plugins/grustoragedb/modules/broadcast/core/init_core_grusotragedb-broadcast.sql
+++ src/sql/plugins/grustoragedb/modules/broadcast/core/init_core_grusotragedb-broadcast.sql
@@ -4,7 +4,7 @@
 --
 DELETE FROM core_admin_right WHERE id_right = 'GRUSTORAGEDB_BROADCAST_MANAGEMENT';
 INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is_updatable,plugin_name,id_feature_group,icon_url,documentation_url, id_order ) VALUES 
-('GRUSTORAGEDB_BROADCAST_MANAGEMENT','grustoragedb-broadcast.adminFeature.ManageSubscriptions.name',1,'jsp/admin/plugins/grustoragedb-broadcast/ManageSubscriptions.jsp','grustoragedb-broadcast.adminFeature.ManageSubscriptions.description',0,'grustoragedb-broadcast',NULL,NULL,NULL,4);
+('GRUSTORAGEDB_BROADCAST_MANAGEMENT','grustoragedb-broadcast.adminFeature.ManageSubscriptions.name',1,'jsp/admin/plugins/grustoragedb/modules/broadcast/ManageSubscriptions.jsp?plugin_name=grustoragedb-broadcast','grustoragedb-broadcast.adminFeature.ManageSubscriptions.description',0,'grustoragedb-broadcast','APPLICATIONS',NULL,NULL,4);
 
 
 --
